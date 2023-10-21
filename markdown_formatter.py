6134b8894cceb3a7ad7a9051527e08d7b8341bd7
John A <johnaziz269@gmail.com>
2023-10-21 21:52:38 +0300

    format first two lines

---
+++ markdown_formatter.py
@@ -0,0 +1,44 @@
+import os
+import string
+
+tutorials = {}
+
+def format_first_two_lines(filepath: string) -> None:
+    """Function that formats the first 2 lines in a tutorial exercise"""
+    with open(filepath, 'r', encoding="utf-8") as file:
+        filedata = file.read()
+        line1, line2 = filedata.split('\n')[:2]
+
+    if line1 != "<!-- markdownlint-disable MD041 -->":
+        filedata = '<!-- markdownlint-disable MD041 -->\n\n' + filedata
+        # Write the file out again
+        with open(filepath, 'w', encoding="utf-8") as file:
+            file.write(filedata)
+    elif line2 not in ['\n', '\r\n', '', ' ']:
+        filedata = filedata.replace('<!-- markdownlint-disable MD041 -->', 
+                                    '<!-- markdownlint-disable MD041 -->\n')
+        # Write the file out again
+        with open(filepath, 'w', encoding="utf-8") as file:
+            file.write(filedata)
+    else:
+        pass
+
+# get tutorial folders
+path = './docs/dev/tutorials/'
+for item in os.listdir(path):
+    if os.path.isdir(os.path.join(path, item)):
+        tutorials[item] = []
+
+
+# get tutorial exercises (includes folder content)
+for tutorial in tutorials.keys():
+    for item in os.listdir(os.path.join(path, tutorial + '/includes/')):
+        tutorials[tutorial].append(item)
+
+
+# iterate over the files to validate content
+for tutorial_folder_name, tutorial_exercises in tutorials.items():
+    for tutorial_exercise in tutorial_exercises:
+        markdown_file_path = os.path.join(path, tutorial_folder_name+ '/includes/'+tutorial_exercise)
+        format_first_two_lines(markdown_file_path)
+        
